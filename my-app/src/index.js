6561be259327860e591722ce93ffb0eb1c1244b6
Stephanie <40613732+steph-bot@users.noreply.github.com>
2023-01-03 12:17:44 -0600

    working tic-tac-toe game

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,21 +1,60 @@
-class Square extends React.Component {
-    render() {
-      return (
-        <button className="square">
-          {/* TODO */}
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+// function component: simpler way to write components that only contains
+// a render method and don't have their own state.
+//
+// takes props as an input and returns what should be rendered.
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
         </button>
+    )
+}
+
+
+  
+class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true,
+        };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+
+    renderSquare(i) {
+      return (
+        <Square
+            value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}
+        />
       );
     }
-  }
-  
-  class Board extends React.Component {
-    renderSquare(i) {
-      return <Square />;
-    }
   
     render() {
-      const status = 'Next player: X';
-  
+    //   const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+        status = 'Winner: ' + winner;
+    } else {
+        status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
       return (
         <div>
           <div className="status">{status}</div>
@@ -37,9 +76,9 @@ class Square extends React.Component {
         </div>
       );
     }
-  }
+}
   
-  class Game extends React.Component {
+class Game extends React.Component {
     render() {
       return (
         <div className="game">
@@ -53,10 +92,30 @@ class Square extends React.Component {
         </div>
       );
     }
+}
+  
+// ========================================
+  
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
+
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
   }
-  
-  // ========================================
-  
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(<Game />);
   
